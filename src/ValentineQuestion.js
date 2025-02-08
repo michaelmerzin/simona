ef6341d1acb76220dc34ef2b6266ed217c2ebacb
michaelmerzin <merzin03@gmail.com>
2025-02-08 17:00:25 +0200

    Fixed build issues2

--- src/ValentineQuestion.js
+++ src/ValentineQuestion.js
@@ -6,7 +6,27 @@ const ValentineQuestion = ({ onAnswer }) => {
   const yourImageArray = useMemo(() => [
     require('./images/IMG_2672.jpg'),
     require('./images/IMG_2673.jpg'),
-    require('./images/IMG_2684.jpg')
+    require('./images/IMG_2684.jpg'),
+    require('./images/IMG_2687.jpg'),
+    require('./images/IMG_2688.jpg'),
+    require('./images/IMG_2689.jpg'),
+    require('./images/IMG_2779.jpg'),
+    require('./images/IMG_2800.jpg'),
+    require('./images/IMG_2811.jpg'),
+    require('./images/IMG_3055.jpg'),
+    require('./images/IMG_3055.jpg'),
+    require('./images/IMG_3056.jpg'),
+    require('./images/IMG_3057.jpg'),
+    require('./images/IMG_3058.jpg'),
+    require('./images/IMG_3059.jpg'),
+    require('./images/IMG_3060.jpg'),
+    require('./images/IMG_3061.jpg'),
+    require('./images/IMG_3062.jpg'),
+    require('./images/IMG_3063.jpg'),
+    require('./images/IMG_3064.jpg'),
+
+
+
     // Add more image paths as needed
   ], []);
   
